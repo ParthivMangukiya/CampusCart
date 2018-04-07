0ac95f28aa391fcfee0410d0db572374e79710b7
rushilapatel <rushilapatel98@gmail.com>
2018-04-07 18:40:22 +0530

    Api modified

--- api/include/DbOperation.php
+++ api/include/DbOperation.php
@@ -10,6 +10,48 @@ class DbOperation {
         $this->con = $db->connect();
     }
 
+    public function createUser($name,$email, $mobile_no, $photo_url, $date_of_registration, $uid) {
+        $res = [];
+        if (!($this->isUserExists($uid))) {
+            $stmt = $this->con->prepare("INSERT INTO user(name, email, mobile_no, photo_url, date_of_registration, firebase_uid) values(? ,?, ?, ?, ?, ?)");
+            $stmt->bind_param("ssssss",$name, $email, $mobile_no, $photo_url, $date_of_registration, $uid);
+            $result = $stmt->execute();
+            $res['id'] = $stmt->insert_id;
+            $stmt->close();
+            if ($result) {
+                $res['error'] = 0;
+            } else {
+                $res['error'] = 1;
+            }
+        } else {
+            $res['error'] = 2;
+        }
+
+
+        return $res;
+    }
+
+    public function getUserByFirebaseUserId($id) {
+        $stmt = $this->con->prepare("SELECT * FROM user WHERE firebase_uid = ?");
+        if($stmt){
+            $stmt->bind_param("s", $id);
+            $stmt->execute();
+            $student = $stmt->get_result();
+            $stmt->close();
+            return $student;
+        }else{
+            return false;
+        }
+    }
+
+    public function getAllUsers() {
+        $stmt = $this->con->prepare("SELECT * FROM user");
+        $stmt->execute();
+        $students = $stmt->get_result();
+        $stmt->close();
+        return $students;
+    }
+
     public function getAllItems() {
         $stmt = $this->con->prepare("SELECT * FROM item");
         $stmt->execute();
@@ -27,11 +69,11 @@ class DbOperation {
     }
 
 
-    public function createItem($price, $name, $category, $description) {
+    public function createItem($price, $name, $category, $description, $url) {
         $res = [];
         if (!($this->isItemExists($name))) {
-            $stmt = $this->con->prepare("INSERT INTO item(price, name, category, description) values(?, ?, ?, ?)");
-            $stmt->bind_param("ssss",$price, $name, $category, $description);
+            $stmt = $this->con->prepare("INSERT INTO item(price, name, category, description,imageUri) values(?, ?, ?, ?, ?)");
+            $stmt->bind_param("sssss",$price, $name, $category, $description, $url);
             $result = $stmt->execute();
             $res['id'] = $stmt->insert_id;
             $stmt->close();
@@ -50,6 +92,20 @@ class DbOperation {
 
     private function isItemExists($firebase_uid) {
         $stmt = $this->con->prepare("SELECT * from item WHERE name = ?");
+        if ($stmt) {
+            $stmt->bind_param("s", $firebase_uid);
+            $stmt->execute();
+            $stmt->store_result();
+            $num_rows = $stmt->num_rows;
+            $stmt->close();
+            return $num_rows > 0;
+        } else {
+            return true;
+        }
+    }
+
+    private function isUserExists($firebase_uid) {
+        $stmt = $this->con->prepare("SELECT user_id from user WHERE firebase_uid = ?");
         if ($stmt) {
             $stmt->bind_param("s", $firebase_uid);
             $stmt->execute();
