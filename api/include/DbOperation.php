06c6fcac608ad2f7e4d5385f0094d8004c6657e5
rushilapatel <rushilapatel98@gmail.com>
2018-04-08 06:47:03 +0530

    API change

--- api/include/DbOperation.php
+++ api/include/DbOperation.php
@@ -12,7 +12,7 @@ class DbOperation {
 
     public function createUser($name,$email, $mobile_no, $photo_url, $date_of_registration, $uid) {
         $res = [];
-        if (!($this->isUserExists($uid))) {
+        if (!($this->isUserExists($email))) {
             $stmt = $this->con->prepare("INSERT INTO user(name, email, mobile_no, photo_url, date_of_registration, firebase_uid) values(? ,?, ?, ?, ?, ?)");
             $stmt->bind_param("ssssss",$name, $email, $mobile_no, $photo_url, $date_of_registration, $uid);
             $result = $stmt->execute();
@@ -35,6 +35,58 @@ class DbOperation {
         $stmt = $this->con->prepare("SELECT * FROM user WHERE firebase_uid = ?");
         if($stmt){
             $stmt->bind_param("s", $id);
+            $stmt->execute();
+            $student = $stmt->get_result();
+            $stmt->close();
+            return $student;
+        }else{
+            return false;
+        }
+    }
+
+    public function getUserByUserId($id) {
+        $stmt = $this->con->prepare("SELECT * FROM user WHERE id = ?");
+        if($stmt){
+            $stmt->bind_param("s", $id);
+            $stmt->execute();
+            $student = $stmt->get_result();
+            $stmt->close();
+            return $student;
+        }else{
+            return false;
+        }
+    }
+
+    public function getUserIdByEmailId($email) {
+        $stmt = $this->con->prepare("SELECT id FROM user WHERE email = ?");
+        if($stmt){
+            $stmt->bind_param("s", $id);
+            $stmt->execute();
+            $student = $stmt->get_result();
+            $stmt->close();
+            return $student;
+        }else{
+            return false;
+        }
+    }
+
+    public function getOrderById($id) {
+        $stmt = $this->con->prepare("SELECT * FROM orders WHERE id = ?");
+        if($stmt){
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
+            $student = $stmt->get_result();
+            $stmt->close();
+            return $student;
+        }else{
+            return false;
+        }
+    }
+
+    public function getItemIdByOrderId($id) {
+        $stmt = $this->con->prepare("SELECT iid FROM order_items WHERE id = ?");
+        if($stmt){
+            $stmt->bind_param("i", $id);
             $stmt->execute();
             $student = $stmt->get_result();
             $stmt->close();
@@ -90,6 +142,42 @@ class DbOperation {
         return $res;
     }
 
+    public function createOrder($uid, $otp, $amount, $color) {
+        $res = [];
+       
+            $stmt = $this->con->prepare("INSERT INTO orders(uid, otp, amount, color) values(?, ?, ?, ?)");
+            $stmt->bind_param("iidi",$uid, $otp, $amount,$color);
+            $result = $stmt->execute();
+            $res['id'] = $stmt->insert_id;
+            $stmt->close();
+            if ($result) {
+                $res['error'] = 0;
+            } else {
+                $res['error'] = 1;
+            }
+        
+
+        return $res;
+    }
+
+    public function createOrderItems($id, $iid) {
+        $res = [];
+       		
+            $stmt = $this->con->prepare("INSERT INTO order_items(id, iid) values(?, ?)");
+            $stmt->bind_param("ii",$id, $iid);
+            $result = $stmt->execute();
+            //$res['id'] = $stmt->insert_id;
+            $stmt->close();
+            if ($result) {
+                $res['error'] = 0;
+            } else {
+                $res['error'] = 1;
+            }
+        
+
+        return $res;
+    }
+
     private function isItemExists($firebase_uid) {
         $stmt = $this->con->prepare("SELECT * from item WHERE name = ?");
         if ($stmt) {
@@ -105,11 +193,12 @@ class DbOperation {
     }
 
     private function isUserExists($firebase_uid) {
-        $stmt = $this->con->prepare("SELECT id from user WHERE firebase_uid = ?");
+        $stmt = $this->con->prepare("SELECT email from user WHERE email = ?");
         if ($stmt) {
             $stmt->bind_param("s", $firebase_uid);
             $stmt->execute();
             $stmt->store_result();
+
             $num_rows = $stmt->num_rows;
             $stmt->close();
             return $num_rows > 0;
